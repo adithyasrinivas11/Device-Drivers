57913db3298dcf1b910bcc2b6032b6413d35acec
adithya srinivas <adithyasrinivas11@gmail.com>
2021-03-21 19:09:35 +0530

    lab2

--- lab2/Q6/chardev.c
+++ lab2/Q6/chardev.c
@@ -1,24 +1,11 @@
-/*
- * chardev.c: Creates a read-only char device that says how many times you've
- * read from the dev file.
- *
- * You can have some fun with this by removing the module_get/put calls,
- * allowing the module to be removed while the file is still open.
- *
- * Compile with `make`. Load with `sudo insmod chardev.ko`. Check `dmesg | tail`
- * output to see the assigned device number and command to create a device file.
- *
- * From TLDP.org's LKMPG book.
- */
+
 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
-#include <asm/uaccess.h> /* for put_user */
+#include <asm/uaccess.h> 
 
-/*
- * Prototypes - this would normally go in a .h file
- */
+
 
 int init_module(void);
 void cleanup_module(void);
@@ -31,9 +18,6 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 #define DEVICE_NAME "chardev"
 #define BUF_LEN 80
 
-/*
- * Global variables are declared as static, so are global within the file.
- */
 
 static int Major;
 static int Device_Open = 0;
@@ -47,9 +31,7 @@ static struct file_operations fops = {
   .release = device_release
 };
 
-/*
- * This function is called when the module is loaded
- */
+
 int init_module(void)
 {
   Major = register_chrdev(0, DEVICE_NAME, &fops);
@@ -69,25 +51,15 @@ int init_module(void)
   return SUCCESS;
 }
 
-/*
- * This function is called when the module is unloaded
- */
+
 void cleanup_module(void)
 {
-  /*
-   * Unregister the device
-   */
+ 
   unregister_chrdev(Major, DEVICE_NAME);
 }
 
-/*
- * Methods
- */
 
-/*
- * Called when a process tries to open the device file, like
- * "cat /dev/mycharfile"
- */
+
 static int device_open(struct inode *inode, struct file *filp)
 {
   static int counter = 0;
@@ -106,9 +78,7 @@ static int device_open(struct inode *inode, struct file *filp)
   return SUCCESS;
 }
 
-/*
- * Called when a process closes the device file.
- */
+
 static int device_release(struct inode *inode, struct file *filp)
 {
   Device_Open--;
@@ -124,49 +94,31 @@ static int device_release(struct inode *inode, struct file *filp)
   return SUCCESS;
 }
 
-/*
- * Called when a process, which already opened the dev file, attempts to read
- * from it.
- */
+
 static ssize_t device_read(struct file *filp, /* see include/linux/fs.h   */
                            char *buffer,      /* buffer to fill with data */
                            size_t length,     /* length of the buffer     */
                            loff_t *offset)
 {
-  /*
-   * Number of bytes actually written to the buffer
-   */
+
   int bytes_read = 0;
 
-  /*
-   * If we're at the end of the message, return 0 signifying end of file.
-   */
+
   if (*msg_Ptr == 0)
     return 0;
 
-  /*
-   * Actually put the data into the buffer
-   */
   while (length && *msg_Ptr) {
-    /*
-     * The buffer is in the user data segment, not the kernel segment so "*"
-     * assignment won't work. We have to use put_user which copies data from the
-     * kernel data segment to the user data segment.
-     */
+   
     put_user(*(msg_Ptr++), buffer++);
     length--;
     bytes_read++;
   }
 
-  /*
-   * Most read functions return the number of bytes put into the buffer
-   */
+
   return bytes_read;
 }
 
-/*
- * Called when a process writes to dev file: echo "hi" > /dev/hello
- */
+
 static ssize_t
 device_write(struct file *filp, const char *buf, size_t len, loff_t *off)
 {
